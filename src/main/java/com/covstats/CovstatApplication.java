2c934120b086d79cdb16a9ce6197426a238c161c
midosdm <mohamedsaidem01@gmail.com>
2022-02-01 13:53:02 +0100

    initialized project

---
+++ src/main/java/com/covstats/CovstatApplication.java
@@ -0,0 +1,13 @@
+package com.covstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovstatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovstatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/covstats/CovstatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovstatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
