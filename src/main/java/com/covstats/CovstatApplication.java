e7e409b1ce08f049cd0cc8326f85bab9e6d16c4f
midosdm <mohamedsaidem01@gmail.com>
2022-02-03 17:47:44 +0100

    added swagger

--- src/main/java/com/covstats/CovstatApplication.java
+++ src/main/java/com/covstats/CovstatApplication.java
@@ -3,24 +3,12 @@ package com.covstats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//implements CommandLineRunner
 @SpringBootApplication
 @EnableScheduling
 public class CovstatApplication {
-//    @Autowired
-//    private CSVService csvService;
     public static void main(String[] args) {
         SpringApplication.run(CovstatApplication.class, args);
     }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        try {
-//            URL url = new URL("https://coronavirus.politologue.com/data/coronavirus/coronacsv.aspx?format=csv&t=pays");
-//            csvService.save(url);
-//        }catch(MalformedURLException mue){
-//            mue.getMessage();
-//        }
-//    }
 }
