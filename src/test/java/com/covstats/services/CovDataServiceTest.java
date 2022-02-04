47d6551b56384bab338f4538be2ddef334ea8197
midosdm <mohamedsaidem01@gmail.com>
2022-02-04 17:41:45 +0100

    added test

--- src/test/java/com/covstats/services/CovDataServiceTest.java
+++ src/test/java/com/covstats/services/CovDataServiceTest.java
@@ -1,10 +1,20 @@
 package com.covstats.services;
 
+import com.covstats.entities.CovData;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CovDataServiceTest {
+
+    @Autowired
+    private CovDataService covDataService;
 
     @Test
     void save() {
@@ -19,6 +29,13 @@ class CovDataServiceTest {
     }
 
     @Test
-    void getByPaysToday() {
+    void itShouldReturnCountryDataForToday() {
+        //given
+        String pays = "France";
+        //when
+        CovData expected = covDataService.getByPaysToday(pays);
+        //then
+        assertNotNull(expected);
+
     }
 }
